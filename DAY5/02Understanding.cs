75a2dd5ae79a5ebb37848a057bac9229571c81c9
Ugochukwu Ngene <98426671+UgoNgene@users.noreply.github.com>
2022-02-01 23:35:08 -0600

    Create 02Understanding.cs

---
+++ DAY5/02Understanding.cs
@@ -0,0 +1,42 @@
+using System;
+namespace UnderstandingTypes
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+       
+            /* 1.
+            Console.WriteLine("Sbyte min value: " + sbyte.MinValue + "\t\t" + "Sbyte max value: " + sbyte.MaxValue);
+            Console.WriteLine("Byte min value: " + byte.MinValue + "\t\t" + "Byte max value: " + byte.MaxValue);
+            Console.WriteLine("Short min value: " + short.MinValue + "\t\t" + "Short max value: " + short.MaxValue);
+            Console.WriteLine("Ushort min value: " + ushort.MinValue + "\t\t" + "Ushort max value: " + ushort.MaxValue);
+            Console.WriteLine("int min value: " + int.MinValue + "\t" + "int max value: " + int.MaxValue);
+            Console.WriteLine("Uint min value: " + uint.MinValue + "\t\t" + "Uint max value: " + uint.MaxValue);
+            Console.WriteLine("Ulong min value: " + ulong.MinValue + "\t\t" + "Ulong max value: " + ulong.MaxValue);
+            Console.WriteLine("float min value: " + float.MinValue + "\t" + "float max value: " + float.MaxValue);
+            Console.WriteLine("Double min value: " + double.MinValue + "\t\t" + "Double max value: " + double.MaxValue);
+            Console.WriteLine("Decimal min value: " + decimal.MinValue + "\t" + "Decimal max value: " + decimal.MaxValue);
+            Console.WriteLine("long min value: " + long.MinValue + "\t\t\t" + "long max value: " + long.MaxValue);
+            */
+
+            /* 2
+            long centuries = 0;
+
+            Console.WriteLine("Input: ");
+            centuries = Convert.ToInt64( Console.ReadLine());
+
+            Console.WriteLine("Output: " + centuries + " centuries = " + (centuries *100) + " years = " 
+                                         + (centuries * 100*365.24) +" days = " + (centuries * 100 * 365.24 * 24) + " hours = "
+                                         + (centuries * 100 * 365.24 * 24*60)+ " minutes = " + (centuries * 100 * 365.24 * 24 * 60 *60) + " seconds = "
+                                         + (centuries * 100 * 365.24 * 24 * 60 * 60*1000) + " milliseconds = "
+                                         + (centuries * 100 * 365.24 * 24 * 60 * 60 * Math.Pow(1000,2)) + " microseconds = "
+                                         + (centuries * 100 * 365.24 * 24 * 60 * 60 * Math.Pow(1000, 3)) + " nanoseconds");
+            */
+
+
+        }
+
+    }
+
+}
